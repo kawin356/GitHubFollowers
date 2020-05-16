084f07e8a29d4c8f145c2573e063f43563ebf015
Kittikawin <kawin356@gmail.com>
2020-05-16 16:30:51 +0700

    create Custom GFButton (GitHubFollowerButton) create Custom GFTextField

---
+++ GitHubFollowers/GFButton.swift
@@ -0,0 +1,36 @@
+//
+//  GFButton.swift
+//  GitHubFollowers
+//
+//  Created by Kittikawin Sontinarakul on 16/5/2563 BE.
+//  Copyright © 2563 Kittikawin Sontinarakul. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        //custom code below
+        configuration()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configuration()
+    }
+    
+    private func configuration() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
