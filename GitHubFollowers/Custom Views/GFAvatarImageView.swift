fc21ffaa07296570b0f0518f82d1796e0ec9030a
Kittikawin <kawin356@gmail.com>
2020-05-20 14:32:10 +0700

    add NSCache Image

--- GitHubFollowers/Custom Views/GFAvatarImageView.swift
+++ GitHubFollowers/Custom Views/GFAvatarImageView.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class GFAvatarImageView: UIImageView {
+    
+    let cache = NetworkManager.shared.cache
 
     let placeholderImage = UIImage(named: "avatar-placeholder")!
     
@@ -31,6 +33,13 @@ class GFAvatarImageView: UIImageView {
     }
     
     func downloadImage(from url: String) {
+        
+        let cacheKey = NSString(string: url)
+        if let image = cache.object(forKey: cacheKey) {
+            self.image = image
+            return
+        }
+        
         guard let url = URL(string: url) else {
             return
         }
@@ -42,6 +51,7 @@ class GFAvatarImageView: UIImageView {
             guard let data = data else { return }
             
             guard let image = UIImage(data: data) else { return }
+            self.cache.setObject(image, forKey: cacheKey)
             
             DispatchQueue.main.async {
                 self.image = image
